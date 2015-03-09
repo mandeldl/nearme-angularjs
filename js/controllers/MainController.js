69328cdf8e5808b20b0c83363766da7afe11102e
David Mandel <MandelDL@gmail.com>
2015-03-09 16:27:19 -0400

    include helper function to parse the geodata

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,9 +1,12 @@
-app.controller('MainController', ['$scope', 'places', function($scope) {
+app.controller('MainController', ['$scope', 'places', function($scope, places) {
 	$scope.mapCenter = {
 	  lat: 40.741934,
 	  lng: -74.004897,
 	  zoom: 17
 	};
-	$scope.geodata = places.success(data);
-	$scope.mapMarkers = geodataToMarkers($scope.geodata);
+
+	places.success(function(data) {
+		$scope.geodata = data;
+		$scope.mapMarkers = geodataToMarkers($scope.geodata);
+	});
 }]);
