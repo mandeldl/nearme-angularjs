8f98e0fd7034e9cc5f0b76c2c5c24ba9b9efb164
David Mandel <MandelDL@gmail.com>
2015-03-09 15:16:32 -0400

    add places as dependency in maincontroller

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,8 +1,8 @@
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', 'places', function($scope) {
 	$scope.mapCenter = {
 	  lat: 40.741934,
 	  lng: -74.004897,
 	  zoom: 17
 	};
-	
+
 }]);
